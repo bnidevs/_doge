0c27fda8138075a430de018f496baac83f874094
Andrew Shao <ashao1@stuy.edu>
2018-04-19 09:21:34 -0400

    Implemented a few methods on le Kachoooooooooooooooooo

--- QQKachoo.java
+++ QQKachoo.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class QQKachoo<T> implements Deque<T>{
 
@@ -7,5 +8,34 @@ public class QQKachoo<T> implements Deque<T>{
     public T getFirst() {
 	return _que.get( 0 );
     }
+
+    public T getLast() {
+	return _que.get( _que.size() - 1 );
+    }
+
+    public int size() {
+	return _que.size();
+    }
+
+    public boolean add( T x ) {
+	return _que.add( x );
+    }
+
+    public  void addFirst( T x ) {
+	_que.add( 0, x );
+    }
+
+    public T remove() {
+	return _que.remove( 0 );
+    }
+
+    public T removeLast() {
+	return _que.remove( _que.size() - 1 );
+    }
+
+    public Iterator<T> iterator() {
+	
+    }
+	
     public static void main(String[] args){}
 }
